2dea6fbeb0bbac437ba4f92c52d830fc9f70ee06
hiscloan <thorin.pickard@gmail.com>
2025-06-26 17:55:50 +0200

    Updated settings.py

--- raincheck/settings.py
+++ raincheck/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['13.53.89.143']
+ALLOWED_HOSTS = ['13.53.89.143', 'localhost', '127.0.0.1']
 
 
 # Application definition
